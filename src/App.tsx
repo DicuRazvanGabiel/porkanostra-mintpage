8d827fc137463ecaf92a1b6024539cabb910f79a
Razvan Dicu <razvan.dicu@busymachine.com>
2021-12-10 14:56:33 +0200

    Mint page ui

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,10 @@ import {
 import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
 import { createTheme, ThemeProvider } from "@material-ui/core";
 
+import porkaGif from './porka.gif'
+import leftImage from './left.png'
+import rightImage from './rigth.png'
+
 const treasury = new anchor.web3.PublicKey(
   process.env.REACT_APP_TREASURY_ADDRESS!
 );
@@ -44,28 +48,28 @@ const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
 const txTimeout = 30000; // milliseconds (confirm this works for your project)
 
 const theme = createTheme({
-    palette: {
-        type: 'dark',
+  palette: {
+    type: 'dark',
+  },
+  overrides: {
+    MuiButtonBase: {
+      root: {
+        justifyContent: 'flex-start',
+      },
     },
-    overrides: {
-        MuiButtonBase: {
-            root: {
-                justifyContent: 'flex-start',
-            },
-        },
-        MuiButton: {
-            root: {
-                textTransform: undefined,
-                padding: '12px 16px',
-            },
-            startIcon: {
-                marginRight: 8,
-            },
-            endIcon: {
-                marginLeft: 8,
-            },
-        },
+    MuiButton: {
+      root: {
+        textTransform: undefined,
+        padding: '12px 16px',
+      },
+      startIcon: {
+        marginRight: 8,
+      },
+      endIcon: {
+        marginLeft: 8,
+      },
     },
+  },
 });
 
 const App = () => {
@@ -73,32 +77,38 @@ const App = () => {
 
   const wallets = useMemo(
     () => [
-        getPhantomWallet(),
-        getSlopeWallet(),
-        getSolflareWallet(),
-        getSolletWallet({ network }),
-        getSolletExtensionWallet({ network })
+      getPhantomWallet(),
+      getSlopeWallet(),
+      getSolflareWallet(),
+      getSolletWallet({ network }),
+      getSolletExtensionWallet({ network })
     ],
     []
   );
 
   return (
-      <ThemeProvider theme={theme}>
-        <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect={true}>
-            <WalletDialogProvider>
-              <Home
-                candyMachineId={candyMachineId}
-                config={config}
-                connection={connection}
-                startDate={startDateSeed}
-                treasury={treasury}
-                txTimeout={txTimeout}
-              />
-            </WalletDialogProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect={true}>
+          <WalletDialogProvider>
+            {/* <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}> */}
+            <Home
+              candyMachineId={candyMachineId}
+              config={config}
+              connection={connection}
+              startDate={startDateSeed}
+              treasury={treasury}
+              txTimeout={txTimeout}
+            />
+            {/* <img src={leftImage} alt="Porka Nostra" style={{ paddingLeft: '200px', height: '700px', transform: 'scaleX(-1)', marginTop: '70px' }} /> */}
+            {/* </div> */}
+
+
+
+          </WalletDialogProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </ThemeProvider >
   );
 };
 
--- src/Home.tsx
+++ src/Home.tsx
@@ -10,12 +10,18 @@ import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { WalletDialogButton } from "@solana/wallet-adapter-material-ui";
+import leftImage from './left.png'
+import porkaGif from './porka.gif'
+
+// import {
+//   CandyMachine,
+//   awaitTransactionSignatureConfirmation,
+//   getCandyMachineState,
+//   mintOneToken,
+//   shortenAddress,
+// } from "./candy-machine";
 
 import {
-  CandyMachine,
-  awaitTransactionSignatureConfirmation,
-  getCandyMachineState,
-  mintOneToken,
   shortenAddress,
 } from "./candy-machine";
 
@@ -55,100 +61,100 @@ const Home = (props: HomeProps) => {
   const [startDate, setStartDate] = useState(new Date(props.startDate));
 
   const wallet = useAnchorWallet();
-  const [candyMachine, setCandyMachine] = useState<CandyMachine>();
+  // const [candyMachine, setCandyMachine] = useState<CandyMachine>();
 
-  const refreshCandyMachineState = () => {
-    (async () => {
-      if (!wallet) return;
+  // const refreshCandyMachineState = () => {
+  //   (async () => {
+  //     if (!wallet) return;
 
-      const {
-        candyMachine,
-        goLiveDate,
-        itemsAvailable,
-        itemsRemaining,
-        itemsRedeemed,
-      } = await getCandyMachineState(
-        wallet as anchor.Wallet,
-        props.candyMachineId,
-        props.connection
-      );
+  //     const {
+  //       candyMachine,
+  //       goLiveDate,
+  //       itemsAvailable,
+  //       itemsRemaining,
+  //       itemsRedeemed,
+  //     } = await getCandyMachineState(
+  //       wallet as anchor.Wallet,
+  //       props.candyMachineId,
+  //       props.connection
+  //     );
 
-      setItemsAvailable(itemsAvailable);
-      setItemsRemaining(itemsRemaining);
-      setItemsRedeemed(itemsRedeemed);
+  //     setItemsAvailable(itemsAvailable);
+  //     setItemsRemaining(itemsRemaining);
+  //     setItemsRedeemed(itemsRedeemed);
 
-      setIsSoldOut(itemsRemaining === 0);
-      setStartDate(goLiveDate);
-      setCandyMachine(candyMachine);
-    })();
-  };
+  //     setIsSoldOut(itemsRemaining === 0);
+  //     setStartDate(goLiveDate);
+  //     setCandyMachine(candyMachine);
+  //   })();
+  // };
 
-  const onMint = async () => {
-    try {
-      setIsMinting(true);
-      if (wallet && candyMachine?.program) {
-        const mintTxId = await mintOneToken(
-          candyMachine,
-          props.config,
-          wallet.publicKey,
-          props.treasury
-        );
+  // const onMint = async () => {
+  //   try {
+  //     setIsMinting(true);
+  //     if (wallet && candyMachine?.program) {
+  //       const mintTxId = await mintOneToken(
+  //         candyMachine,
+  //         props.config,
+  //         wallet.publicKey,
+  //         props.treasury
+  //       );
 
-        const status = await awaitTransactionSignatureConfirmation(
-          mintTxId,
-          props.txTimeout,
-          props.connection,
-          "singleGossip",
-          false
-        );
+  //       const status = await awaitTransactionSignatureConfirmation(
+  //         mintTxId,
+  //         props.txTimeout,
+  //         props.connection,
+  //         "singleGossip",
+  //         false
+  //       );
 
-        if (!status?.err) {
-          setAlertState({
-            open: true,
-            message: "Congratulations! Mint succeeded!",
-            severity: "success",
-          });
-        } else {
-          setAlertState({
-            open: true,
-            message: "Mint failed! Please try again!",
-            severity: "error",
-          });
-        }
-      }
-    } catch (error: any) {
-      // TODO: blech:
-      let message = error.msg || "Minting failed! Please try again!";
-      if (!error.msg) {
-        if (error.message.indexOf("0x138")) {
-        } else if (error.message.indexOf("0x137")) {
-          message = `SOLD OUT!`;
-        } else if (error.message.indexOf("0x135")) {
-          message = `Insufficient funds to mint. Please fund your wallet.`;
-        }
-      } else {
-        if (error.code === 311) {
-          message = `SOLD OUT!`;
-          setIsSoldOut(true);
-        } else if (error.code === 312) {
-          message = `Minting period hasn't started yet.`;
-        }
-      }
+  //       if (!status?.err) {
+  //         setAlertState({
+  //           open: true,
+  //           message: "Congratulations! Mint succeeded!",
+  //           severity: "success",
+  //         });
+  //       } else {
+  //         setAlertState({
+  //           open: true,
+  //           message: "Mint failed! Please try again!",
+  //           severity: "error",
+  //         });
+  //       }
+  //     }
+  //   } catch (error: any) {
+  //     // TODO: blech:
+  //     let message = error.msg || "Minting failed! Please try again!";
+  //     if (!error.msg) {
+  //       if (error.message.indexOf("0x138")) {
+  //       } else if (error.message.indexOf("0x137")) {
+  //         message = `SOLD OUT!`;
+  //       } else if (error.message.indexOf("0x135")) {
+  //         message = `Insufficient funds to mint. Please fund your wallet.`;
+  //       }
+  //     } else {
+  //       if (error.code === 311) {
+  //         message = `SOLD OUT!`;
+  //         setIsSoldOut(true);
+  //       } else if (error.code === 312) {
+  //         message = `Minting period hasn't started yet.`;
+  //       }
+  //     }
 
-      setAlertState({
-        open: true,
-        message,
-        severity: "error",
-      });
-    } finally {
-      if (wallet) {
-        const balance = await props.connection.getBalance(wallet.publicKey);
-        setBalance(balance / LAMPORTS_PER_SOL);
-      }
-      setIsMinting(false);
-      refreshCandyMachineState();
-    }
-  };
+  //     setAlertState({
+  //       open: true,
+  //       message,
+  //       severity: "error",
+  //     });
+  //   } finally {
+  //     if (wallet) {
+  //       const balance = await props.connection.getBalance(wallet.publicKey);
+  //       setBalance(balance / LAMPORTS_PER_SOL);
+  //     }
+  //     setIsMinting(false);
+  //     refreshCandyMachineState();
+  //   }
+  // };
 
   useEffect(() => {
     (async () => {
@@ -159,68 +165,91 @@ const Home = (props: HomeProps) => {
     })();
   }, [wallet, props.connection]);
 
-  useEffect(refreshCandyMachineState, [
-    wallet,
-    props.candyMachineId,
-    props.connection,
-  ]);
+  // useEffect(refreshCandyMachineState, [
+  //   wallet,
+  //   props.candyMachineId,
+  //   props.connection,
+  // ]);
 
   return (
-    <main>
-      {wallet && (
-        <p>Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}</p>
-      )}
+    <div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <main style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
+          <div style={{ textAlign: 'left', fontSize: '25px', fontFamily: 'sans-serif', fontWeight: 'bold' }}>
+            <div style={{ width: '100%', textAlign: 'center' }}>
+              {wallet && (
+                <div style={{ marginBottom: '5px' }}>Wallet: {shortenAddress(wallet.publicKey.toBase58() || "")}</div>
+              )}
 
-      {wallet && <p>Balance: {(balance || 0).toLocaleString()} SOL</p>}
+              {wallet && <div style={{ marginBottom: '5px' }}>Balance: {(balance || 0).toLocaleString()} SOL</div>}
 
-      {wallet && <p>Total Available: {itemsAvailable}</p>}
+              {/* {wallet && <div style={{ marginBottom: '5px' }}>Total Available: {itemsAvailable}</div>} */}
 
-      {wallet && <p>Redeemed: {itemsRedeemed}</p>}
+              {wallet && <div style={{ marginBottom: '5px' }}>Total Available: 9996</div>}
+              {wallet && <div style={{ marginBottom: '5px' }}>Redeemed: {itemsRedeemed}</div>}
 
-      {wallet && <p>Remaining: {itemsRemaining}</p>}
+              {/* {wallet && <div style={{ marginBottom: '5px' }}>Remaining: {itemsRemaining}</div>} */}
+              {wallet && <div style={{ marginBottom: '5px' }}>Remaining: 9996</div>}
+            </div>
+            {wallet && <div style={{ width: '350px', height: '5px', background: '#E9AC0E', marginTop: '60px' }} />}
 
-      <MintContainer>
-        {!wallet ? (
-          <ConnectButton>Connect Wallet</ConnectButton>
-        ) : (
-          <MintButton
-            disabled={isSoldOut || isMinting || !isActive}
-            onClick={onMint}
-            variant="contained"
-          >
-            {isSoldOut ? (
-              "SOLD OUT"
-            ) : isActive ? (
-              isMinting ? (
-                <CircularProgress />
+            <MintContainer>
+              {!wallet ? (
+                <ConnectButton>Connect Wallet</ConnectButton>
               ) : (
-                "MINT"
-              )
-            ) : (
-              <Countdown
-                date={startDate}
-                onMount={({ completed }) => completed && setIsActive(true)}
-                onComplete={() => setIsActive(true)}
-                renderer={renderCounter}
-              />
-            )}
-          </MintButton>
-        )}
-      </MintContainer>
+                <MintButton
+                  style={{ justifyContent: 'center', fontSize: '28px', position: 'absolute', width: '300px', background: '#E22AFE', fontWeight: 'bold', left: '260px', marginTop: '80px' }}
+                  disabled={isSoldOut || isMinting || !isActive}
+                  // onClick={onMint}
+                  variant="outlined"
+                >
+                  {isSoldOut ? (
+                    "SOLD OUT"
+                  ) : isActive ? (
+                    isMinting ? (
+                      <CircularProgress />
+                    ) : (
+                      "MINT SOON"
+                    )
+                  ) : (
+                    <Countdown
+                      date={startDate}
+                      onMount={({ completed }) => completed && setIsActive(true)}
+                      onComplete={() => setIsActive(true)}
+                      renderer={renderCounter}
+                    />
+                  )}
+                </MintButton>
+              )}
+            </MintContainer>
 
-      <Snackbar
-        open={alertState.open}
-        autoHideDuration={6000}
-        onClose={() => setAlertState({ ...alertState, open: false })}
-      >
-        <Alert
-          onClose={() => setAlertState({ ...alertState, open: false })}
-          severity={alertState.severity}
-        >
-          {alertState.message}
-        </Alert>
-      </Snackbar>
-    </main>
+            <Snackbar
+              open={alertState.open}
+              autoHideDuration={6000}
+              onClose={() => setAlertState({ ...alertState, open: false })}
+            >
+              <Alert
+                onClose={() => setAlertState({ ...alertState, open: false })}
+                severity={alertState.severity}
+              >
+                {alertState.message}
+              </Alert>
+            </Snackbar>
+          </div>
+        </main>
+        <img src={leftImage} alt="Porka Nostra" style={{ paddingLeft: '200px', height: '700px', transform: 'scaleX(-1)', marginTop: '70px' }} />
+
+      </div>
+      <div style={{ width: '100%', textAlign: 'center', fontSize: '50px', marginTop: '50px' }}>
+        <Countdown
+          date={1639778400000}
+          onMount={({ completed }) => completed && setIsActive(true)}
+          onComplete={() => setIsActive(true)}
+          daysInHours={false}
+          renderer={renderCounter}
+        />
+      </div>
+    </div>
   );
 };
 
